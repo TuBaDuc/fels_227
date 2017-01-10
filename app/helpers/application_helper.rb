ee6ff455576691938ef4d8cff7d2881d859c9ed0
TuBaDuc <tubaduc@gmail.com>
2017-01-10 09:38:30 +0700

    static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    system_name = t(:system_name)
+    if page_title.empty?
+      system_name
+    else
+      page_title + " | " + system_name
+    end
+  end
 end
