111cf5d111c582e2d18370920a24e6feba5fe94d
TuBaDuc <tubaduc@gmail.com>
2017-01-10 14:26:00 +0700

    login logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t :please_login
+    redirect_to login_url
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t :user_nil_mess
+      redirect_to root_url
+    end
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def check_correct_user
+    unless current_user?(@user)
+      flash[:danger] = t :error_permission_mess
+      redirect_to root_url
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,52 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token, :reset_token
+  before_save   :downcase_email
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  VALID_PHONE_REGEX = /\d[0-9]\)*\z/i
+  validates :phone, presence: true, length: {maximum: 16},
+    format: {with: VALID_PHONE_REGEX}
+  validates :address, presence: true, length: {maximum: 255}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated?attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute :reset_digest, User.digest(reset_token)
+    update_attribute :reset_sent_at, Time.zone.now
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
