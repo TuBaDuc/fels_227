c1c9e7c4863044b814f74228cf50e7fdbd362b97
TuBaDuc <tubaduc@gmail.com>
2017-01-15 09:16:03 +0700

    [admin] edit and delete word and answer

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     root "users#index"
     resources :users, only: [:index, :destroy]
     resources :categories do
-      resources :words
+      resources :words, except: [:index, :show]
     end
   end
   resources :categories, only: [:index, :show] do
