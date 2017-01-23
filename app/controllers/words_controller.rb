57a40f8df63d76bce25615a25ced2d6f611641a0
TuBaDuc <tubaduc@gmail.com>
2017-01-23 14:50:30 +0700

    export to excel 2

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,24 +2,24 @@ class WordsController < ApplicationController
   before_action :logged_in_user
 
   def index
+    @categories = Category.all
+    if !params[:category_id].nil? && params[:category_id].empty?
+      @words = Word.send params[:option], current_user.id
+    elsif category = @categories.find_by(id: params[:category_id])
+      @words = category.words.send params[:option], current_user.id
+    else
+      @words = Word.all
+    end
+
     respond_to do |format|
-      @categories = Category.all
-      if !params[:category_id].nil? && params[:category_id].empty?
-        @words = Word.send params[:option], current_user.id
-      elsif category = @categories.find_by(id: params[:category_id])
-        @words = category.words.send params[:option], current_user.id
-      else
-        @words = Word.all
-      end
-      @words = @words.paginate(page: params[:page],
-        per_page: Settings.word_of_list_limit)
-      format.html
-      format.js
-      format.json do
-        render json: @words,
-          meta: @words.total_pages,
-          meta_key: "total_pages",
-          status: :ok
+      format.any(:html, :js) {
+        @words = @words.paginate(page: params[:page],
+          per_page: Settings.word_of_list_limit)
+      }
+      format.xlsx do
+        render xlsx: "index",
+        filename: "words_#{Time.now.strftime("%Y%m%d%H%M%S")}",
+        disposition: "inline"
       end
     end
   end
